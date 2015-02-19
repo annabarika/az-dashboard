f34d909861e6fc9dfc073df4e7dc008fd6166d28
anna <annabarika@gmail.com>
2015-02-19 20:09:11 +0200

    fullcalendar

--- modules/buyer/app.js
+++ modules/buyer/app.js
@@ -44,37 +44,62 @@ app.run( function($rootScope, $location) {
 });
 
 app.controller('DatepickerCtrl', function ($scope) {
-	$scope.today = function() {
-		$scope.dt = new Date();
-	};
-	$scope.today();
+    $scope.today = function() {
+        $scope.dt = new Date();
+    };
+    $scope.today();
 
-	$scope.clear = function () {
-		$scope.dt = null;
-	};
+    $scope.clear = function () {
+        $scope.dt = null;
+    };
 
-	// Disable weekend selection
-	$scope.disabled = function(date, mode) {
-		return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
-	};
+    // Disable weekend selection
+    $scope.disabled = function(date, mode) {
+        return ( mode === 'day' && ( date.getDay() === 0 || date.getDay() === 6 ) );
+    };
 
-	$scope.toggleMin = function() {
-		$scope.minDate = $scope.minDate ? null : new Date();
-	};
-	$scope.toggleMin();
 
-	$scope.open = function($event) {
-		$event.preventDefault();
-		$event.stopPropagation();
+    $scope.$watch('dtMin',function(newVal){
+        console.log(newVal);
+    });
 
-		$scope.opened = true;
-	};
+    $scope.$watch('dtMax',function(newVal){
+        console.log(newVal);
+    });
 
-	$scope.dateOptions = {
-		formatYear: 'yy',
-		startingDay: 1
-	};
+    $scope.$watch('dt',function(newVal){
+        console.log(newVal);
+    });
 
-	$scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
-	$scope.format = $scope.formats[0];
+    $scope.open = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.opened = true;
+    };
+
+
+    $scope.openMin = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.openedMin = true;
+    };
+
+    $scope.openMax = function($event) {
+        $event.preventDefault();
+        $event.stopPropagation();
+
+        $scope.openedMax = true;
+    };
+
+
+    $scope.dateOptions = {
+        formatYear: 'yy',
+        startingDay: 1
+    };
+
+    $scope.formats = ['dd-MMMM-yyyy', 'yyyy/MM/dd', 'dd.MM.yyyy', 'shortDate'];
+    $scope.format = $scope.formats[0];
 });
+
