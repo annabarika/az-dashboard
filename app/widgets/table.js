44c5443f99a318a8953a1bd79c4417d4ec0723b6
Niklucky <iam@niklucky.com>
2015-02-20 00:02:54 +0200

    Table widget custom columns in Payment

--- app/widgets/table.js
+++ app/widgets/table.js
@@ -8,7 +8,7 @@
     /**
      * Factory for
      */
-    app.factory("tableFactory",["$filter","$rootScope",function($filter,$rootScope){
+    app.factory("tableFactory", ["$filter","$rootScope",function($filter,$rootScope){
 
         var service={};
 
@@ -28,10 +28,77 @@
             $rootScope.method=method;
         };
 
+        service.prepareData = function(header, data){
+
+        };
         return service;
     }]);
 
+app.filter('orderByEx', orderByExFilter);
 
+
+    function orderByExFilter($parse){ // modified version of native Angular orderBy filter
+        return function(array, tablehead, sortPredicate, reverseOrder) {
+            if (!(array instanceof Array)) return array;
+            if (!sortPredicate) return array;
+            sortPredicate = angular.isArray(sortPredicate) ? sortPredicate: [sortPredicate];
+            sortPredicate = map(sortPredicate, function(predicate){
+                var descending = false, list, get = predicate || identity;
+                if (angular.isString(predicate)) {
+                    if ((predicate.charAt(0) == '+' || predicate.charAt(0) == '-')) {
+                        descending = predicate.charAt(0) == '-';
+                        predicate = predicate.substring(1);
+                    }
+                    get = $parse(predicate);
+                }
+                // if list of values specified
+                if (list = find(tablehead,predicate)) {
+                    return reverseComparator(function(a,b){ // return list-based comparator
+                        return compare(list[get(a)],list[get(b)]);
+                    }, descending);
+                }
+                return reverseComparator(function(a,b){ // else use native comparator
+                    return compare(get(a),get(b));
+                }, descending);
+            });
+            var arrayCopy = [];
+            for ( var i = 0; i < array.length; i++) { arrayCopy.push(array[i]); }
+            return arrayCopy.sort(reverseComparator(comparator, reverseOrder));
+
+            function comparator(o1, o2){
+                for ( var i = 0; i < sortPredicate.length; i++) {
+                    var comp = sortPredicate[i](o1, o2);
+                    if (comp !== 0) return comp;
+                }
+                return 0;
+            }
+            function reverseComparator(comp, descending) {
+                return !!(descending)
+                    ? function(a,b){return comp(b,a);}
+                    : comp;
+            }
+            function compare(v1, v2){
+                var t1 = typeof v1;
+                var t2 = typeof v2;
+                if (t1 == t2) {
+                    if (t1 == "string") v1 = v1.toLowerCase();
+                    if (t1 == "string") v2 = v2.toLowerCase();
+                    if (v1 === v2) return 0;
+                    return v1 < v2 ? -1 : 1;
+                } else {
+                    return t1 < t2 ? -1 : 1;
+                }
+            }
+            function map (obj, iterator, context) { // copy of native Angular map function
+                var results = [];
+                angular.forEach(obj, function(value, index, list) {
+                    results.push(iterator.call(context, value, index, list));
+                });
+                return results;
+            }
+        }
+    }
+    orderByExFilter.$inject = ['$parse'];
 
     app.directive("widgetTable", ["tableFactory",function(tableFactory){
         return{
@@ -41,6 +108,7 @@
             //require:"",
             scope:{
                 datarows:"="
+                ,dataheader:"="
                 ,edit:"&"
                 ,row:"="
                 ,toolbarButtons:"="
@@ -49,11 +117,39 @@
             },
 
             link:function($scope){
-console.log($scope.caption);
+
+                $scope.sortBy = function() {
+
+                    //console.log($scope.dataheader);
+
+                    var order = [];
+                    angular.forEach($scope.dataheader, function(h){
+                        if (h.sort>0) order[h.sort-1] = h.name;
+                        if (h.sort<0) order[Math.abs(h.sort)-1] = '-'+h.name;
+                    });
+                    return order;
+                };
+
+                $scope.sortReorder = function(col,e) {
+
+                    if (e.shiftKey) {
+                        var sortIndex = 0;
+                        angular.forEach($scope.dataheader, function(el) {
+                            if (Math.abs(el.sort)>sortIndex) sortIndex = Math.abs(el.sort);
+                        });
+                        angular.forEach($scope.dataheader, function(el) {
+                            if (el.name==col) el.sort = el.sort?-el.sort:sortIndex+1;
+                        });
+                    } else {
+                        angular.forEach($scope.dataheader, function(el) {
+                            if (el.name==col) el.sort = el.sort>0?-1:1; else el.sort = null;
+                        });
+                    }
+                };
                 $scope.column_sorter=function(key){
                     console.log(key);
-                    $scope.sortField=key;
-                    $scope.reverse=!$scope.reverse;
+                    $scope.sortField = key;
+                    $scope.reverse = !$scope.reverse;
                 };
 
                 $scope.row_info=function(row){
--- modules/buyer/controllers/payments.js
+++ modules/buyer/controllers/payments.js
@@ -12,25 +12,57 @@ app.controller('PaymentListController',
 		"$route",
 		"RestFactory",
 
-
 		function ($scope, $rootScope, $modal, $location, $route, RestFactory){
 
 			$scope.$route = $route;
 			$scope.$location = $location;
-			console.log(config.API.host);
-
-			var modalWindow,
-				url,
-				method,
-				data,
-				header;
 
 			/* Getting payments */
 			$rootScope.documentTitle = "Payments";
+			$scope.tableHeader = [
+				{ _id:1, name: "id", title: 'ID' },
+				{ _id:2, name: "orderId", title: 'Order' },
+				{ _id:3, name: "factory", title: 'Factory' },
+				{ _id:4, name: "paymentDate", title: 'Payment date' },
+				{ _id:5, name: "paymentMethod", title: 'Payment method' },
+				{ _id:6, name: "paymentAmount", title: 'Payment' },
+				{ _id:7, name: "refundAmount", title: 'Refund' }
+			];
 
 			RestFactory.request(config.API.host + "payment/load")
 				.then(function(response){
-					$scope.data = response;
+					console.log($scope.tableHeader);
+					//console.log(response);
+					var data = [];
+					//data.header = $scope.paymentsHeader;
+
+					for( var i in response ){
+						data[i] = {};
+						for( var n in $scope.tableHeader ) {
+							//console.log(response[i]);
+							var key = $scope.tableHeader[n].name;
+
+							if( response[i][key]  ) {
+								data[i][n] = response[i][key];
+							}else{
+								data[i][n] = '';
+
+								if(key == 'paymentAmount'){
+									if(response[i].paymentType == 'payment'){
+										data[i][n] = response[i].amount;
+									}
+								}
+								if(key == 'refundAmount'){
+									if(response[i].paymentType == 'refund'){
+										data[i][n] = response[i].amount;
+									}
+								}
+							}
+						}
+					}
+					console.log(data);
+					$scope.data = data;
+
 				});
 
 		}]);
