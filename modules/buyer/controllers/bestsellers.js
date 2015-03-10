7b022a329ee7b13b8764ac1eb8067874246b4ee4
niklucky <iam@niklucky.com>
2015-03-10 17:39:30 +0800

    Bestseller: Ready to create order

--- modules/buyer/controllers/bestsellers.js
+++ modules/buyer/controllers/bestsellers.js
@@ -2,7 +2,11 @@ var app = angular.module("modules.buyer.bestsellers", [
 
 ]);
 
-app.controller('BestsellerItemController', function($scope, $rootScope, $route, $location, RestFactory){
+app.controller('BestsellerItemController',
+    [
+        '$scope', '$rootScope', '$route', '$location', 'RestFactory',
+
+        function($scope, $rootScope, $route, $location, RestFactory){
     $scope.$route = $route;
     $scope.$location = $location;
 
@@ -12,11 +16,18 @@ app.controller('BestsellerItemController', function($scope, $rootScope, $route, 
 
     RestFactory.request( url )
         .then(function(response){
+
             $scope.product = response;
+            $scope.sizes = {};
             $rootScope.documentTitle = $scope.product.brand + ' ('+ $scope.product.articul +')';
 
         },
         function(error){
             console.log(error);
         });
-});
+
+    $scope.createOrder = function(sizes){
+        console.log($scope.product);
+        console.log(sizes);
+    }
+}]);
