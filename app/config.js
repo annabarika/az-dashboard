413d3a7f829c095cdb4fc205650c76f7e7eb7aae
niklucky <iam@niklucky.com>
2015-03-30 16:23:56 +0300

    Api path

--- app/config.js
+++ app/config.js
@@ -1,9 +1,9 @@
 var config = {
     API: {
-        host: "http://lex.f.azimuth/api/",
+        host: "/api/",
         key: 'test',
-        imagehost: 'http://lex.b.compass',
-        getproducts:"http://lex.f.azimuth/api/jsonp.php",
+        imagehost: 'http://compass',
+        getproducts:"/api/jsonp.php",
         tokien_id : "5f77e685beaa564fd3585738d65108c4"
     },
     monthNames: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
