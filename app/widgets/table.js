b2e746cd250d82523d19afafbdc045c6bcbc1b95
StanislavWEB <stanisov@gmail.com>
2015-04-16 17:30:28 +0300

    [FIX] Total for paid payments

--- app/widgets/table.js
+++ app/widgets/table.js
@@ -197,7 +197,7 @@
                                 }
                                 else{
 
-                                    num = parseInt(value);
+                                    num = parseFloat(value);
 
                                     if(num){
 
