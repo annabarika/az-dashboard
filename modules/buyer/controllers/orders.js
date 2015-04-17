b003c999f7dc311e85a4f02d4bf6ba988d949f45
Kostyanuch <kostyanuch87@gmail.com>
2015-04-17 16:53:34 +0200

    [FIX]

--- modules/buyer/controllers/orders.js
+++ modules/buyer/controllers/orders.js
@@ -564,27 +564,48 @@ app.controller('OrderListController',
                         $scope.create=function(factory){
                             console.log(factory);
                             if(!factory){
-                                messageCenterService.add("danger","Please entered form",{timeout:3000});
+                                messageCenterService.add("danger","Please, entered form",{timeout:3000});
                                 return;
                             }
-                            if(factory.groupId==""||!parseInt(factory.groupId)||factory.groupId==undefined){
-                                messageCenterService.add("danger","Group id must be numeric",{timeout:3000});
+                            if(factory.name==""||factory.name==undefined){
+                                messageCenterService.add("danger","Please, enter name field",{timeout:3000});
                                 return;
                             }
-                            if(factory.productionDays==""||!parseInt(factory.productionDays)||factory.productionDays==undefined){
-                                messageCenterService.add("danger","Please enter valid production days count",{timeout:3000});
+                            if(factory.groupId==""||factory.groupId==undefined){
+                                messageCenterService.add("danger","Please, enter group Id field",{timeout:3000});
                                 return;
                             }
-                            if(factory.name==""&&factory.phone==""||factory.name==undefined&&factory.phone==undefined){
-                                messageCenterService.add("danger","Please enter name or phone",{timeout:3000});
+                            if(!parseInt(factory.groupId)){
+                                messageCenterService.add("danger","Please, use only numbers in group Id field",{timeout:3000});
+                                return;
+                            }
+
+                            if(factory.productionDays==""||factory.productionDays==undefined){
+                                messageCenterService.add("danger","Please, enter valid production days count",{timeout:3000});
+                                return;
+                            }
+                            if(!parseInt(factory.productionDays)){
+                                messageCenterService.add("danger","Please, use only numbers in production days field",{timeout:3000});
+                                return;
+                            }
+                            if(factory.phone==""||factory.phone==undefined){
+                                messageCenterService.add("danger","Please, enter phone field",{timeout:3000});
+                                return;
+                            }
+                            if(!parseInt(factory.phone)){
+                                messageCenterService.add("danger","Please, use numbers in phone field",{timeout:3000});
                                 return;
                             }
                             if(factory.email==""||factory.email==undefined){
-                                messageCenterService.add("danger","Please enter email",{timeout:3000});
+                                messageCenterService.add("danger","Please, enter email",{timeout:3000});
                                 return;
                             }
                             if(factory.currencyId==""||factory.currencyId==undefined||!parseInt(factory.currencyId)){
-                                messageCenterService.add("danger","Please enter currency",{timeout:3000});
+                                messageCenterService.add("danger","Please, enter currency",{timeout:3000});
+                                return;
+                            }
+                            if(!parseInt(factory.currencyId)){
+                                messageCenterService.add("danger","Please, use only numbers",{timeout:3000});
                                 return;
                             }
                             modalInstance.close(factory);
